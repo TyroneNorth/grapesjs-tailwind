8dce25f9b7ce3cbcae42de0df1dd748cf880bb5a
Tyrone North <tnorthjr@gmail.com>
2024-03-04 19:54:53 -0600

    update

---
+++ src/blocks/data/accordion-1.js
@@ -0,0 +1,65 @@
+export const source = `<!-- component -->
+<div class="m-2 space-y-2">
+  <div
+    class="group flex flex-col gap-2 rounded-lg bg-black p-5 text-white"
+    tabindex="1"
+  >
+    <div class="flex cursor-pointer items-center justify-between">
+      <span> HTML </span>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/9/96/Chevron-icon-drop-down-menu-WHITE.png"
+        class="h-2 w-3 transition-all duration-500 group-focus:-rotate-180"
+      />
+    </div>
+    <div
+      class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </div>
+  </div>
+
+  <div
+    class="group flex flex-col gap-2 rounded-lg bg-black p-5 text-white"
+    tabindex="2"
+  >
+    <div class="flex cursor-pointer items-center justify-between">
+      <span> CSS </span>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/9/96/Chevron-icon-drop-down-menu-WHITE.png"
+        class="h-2 w-3 transition-all duration-500 group-focus:-rotate-180"
+      />
+    </div>
+    <div
+      class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </div>
+  </div>
+
+  <div
+    class="group flex flex-col gap-2 rounded-lg bg-black p-5 text-white"
+    tabindex="3"
+  >
+    <div class="flex cursor-pointer items-center justify-between">
+      <span> JAVASCRIPT </span>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/9/96/Chevron-icon-drop-down-menu-WHITE.png"
+        class="h-2 w-3 transition-all duration-500 group-focus:-rotate-180"
+      />
+    </div>
+    <div
+      class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </div>
+  </div>
+</div>`;
